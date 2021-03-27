a0d5612bf74f4e3b91dda815a1e31b3c9a310030
Zach Mead <zachary.n.mead@gmail.com>
2021-03-26 19:46:39 -0500

    Part 3

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -4,20 +4,47 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
-    uint public productCount = 0; 
-    mapping(uint => Product) public products;
+    uint256 public productCount = 0;
+    mapping(uint256 => Product) public products;
 
     struct Product {
-        uint id;
+        uint256 id;
         string name;
-        uint price;
+        uint256 price;
         address owner;
-        bool purchased; 
-
+        bool purchased;
     }
+
+    event ProductCreated(
+        uint256 id,
+        string name,
+        uint256 price,
+        address owner,
+        bool purchased
+    );
 
     constructor() public {
         name = "Marketplace";
     }
-}
 
+    function createProduct(string memory _name, uint256 _price) public {
+        // Require a valid name
+        require(bytes(_name).length > 0);
+        // Require a valid price
+        require(_price > 0);
+        // Increment product count
+        productCount++;
+        // Create the product
+        products[productCount] = Product(
+            productCount,
+            _name,
+            _price,
+            msg.sender,
+            false
+        );
+        // Trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    
+}
