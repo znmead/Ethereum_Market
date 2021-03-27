8b0bda4ae0bbab5359739ba79ada73ee68121a68
Zach Mead <zachary.n.mead@gmail.com>
2021-03-27 16:25:08 -0500

    Working, but not unloading

--- src/components/App.js
+++ src/components/App.js
@@ -40,9 +40,17 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       console.log(marketplace)
+
       const productCount = await marketplace.methods.productCount().call()
       console.log(productCount.toString())
-      this.setState({ loading: false })
+      this.setState({ productCount, loading: false })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
     }
@@ -51,13 +59,20 @@ class App extends Component {
   createProduct(name, price) {
     this.setState({ loading: true })
     this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
-      .once('reciept', (reciept) => {
+      .once('receipt', (receipt) => {
+        this.setState({ loading: false })
+      })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+      .once('receipt', (receipt) => {
         this.setState({ loading: false })
       })
   }
 
   constructor(props) {
-
     super(props)
     this.state = {
       account: '',
@@ -66,6 +81,7 @@ class App extends Component {
       loading: true
     }
     this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
 
@@ -78,11 +94,15 @@ class App extends Component {
         <div className="container-fluid bg-dark mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex">
-              <img src={logo} className="App-logo" alt="logo" />
+
               {this.state.loading
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <Main createProduct={this.createProduct} />
+                : <Main
+                  products={this.state.products}
+                  createProduct={this.createProduct}
+                  purchaseProduct={this.purchaseProduct} />
               }
+              <img src={logo} className="App-logo" alt="logo" />
             </main>
           </div>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,75 +2,78 @@ import React, { Component } from 'react';
 
 class Main extends Component {
 
-  render() {
-    return (
-      <div id="content">
-        <h1>Add Product</h1>
-        <form onSubmit={(event) => {
-          event.preventDefault()
-          const name = this.productName.value
-          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
-          this.props.createProduct(name, price)
-        }}>
-          <div className="form-group mr-sm-2">
-            <input
-              id="productName"
-              type="text"
-              ref={(input) => { this.productName = input }}
-              className="form-control"
-              placeholder="Product Name"
-              required />
-          </div>
-          <div className="form-group mr-sm-2">
-            <input
-              id="productPrice"
-              type="text"
-              ref={(input) => { this.productPrice = input }}
-              className="form-control"
-              placeholder="Product Price"
-              required />
-          </div>
-          <button type="submit" className="btn btn-primary">Add Product</button>
-        </form>
-        <p> </p>
-        <h2>Buy Product</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Owner</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody id="productList">
-            <tr>
-              <th scope="row">1</th>
-              <td>iPhone x</td>
-              <td>1 Eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Macbook Pro</td>
-              <td>3 eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Airpods</td>
-              <td>0.5 eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="content">
+                <h1>Add Product</h1>
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    const name = this.productName.value
+                    const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
+                    this.props.createProduct(name, price)
+                }}>
+                    <div className="form-group mr-sm-2">
+                        <input
+                            id="productName"
+                            type="text"
+                            ref={(input) => { this.productName = input }}
+                            className="form-control"
+                            placeholder="Product Name"
+                            required />
+                    </div>
+                    <div className="form-group mr-sm-2">
+                        <input
+                            id="productPrice"
+                            type="text"
+                            ref={(input) => { this.productPrice = input }}
+                            className="form-control"
+                            placeholder="Product Price"
+                            required />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add Product</button>
+                </form>
+                <p> </p>
+                <h2>Buy Product</h2>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Owner</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    {this.props.products.map((product, key) => {
+                        return (
+                            <tbody key= {key}>
+                                <tr key={key}>
+                                    <th scope="row">{product.id.toString()}</th>
+                                    <td>{product.name}</td>
+                                    <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
+                                    <td>{product.owner}</td>
+                                    <td>
+                                        {!product.purchased
+                                            ? <button
+                                                name={product.id}
+                                                value={product.price}
+                                                onClick={(event) => {
+                                                    this.props.purchaseProduct(event.target.name, event.target.value)
+                                                }}
+                                            >
+                                                Buy
+                                            </button>
+                                            : null
+                                        }
+                                    </td>
+                                </tr>
+                            </tbody>
+                        )
+                    })}
+                </table>
+            </div>
+        );
+    }
 }
 
 export default Main;
